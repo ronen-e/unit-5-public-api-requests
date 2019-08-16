084329e3a6c412e2d92038bd8ab417314ed32eff
mayaliberman <50631141+mayaliberman@users.noreply.github.com>
2019-08-16 13:42:28 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,8 @@ const peopleUrl = 'https://randomuser.me/api/?results=12';
 //  FETCH FUNCTIONS
 // ------------------------------------------
 function getProfiles(json) {
-  const profiles = json.results.map(person => {
+  const profileArr = [];
+  json.results.map(person => {
     const firstName = person.name.first;
     const lastName = person.name.last;
     const picture = person.picture.medium;
@@ -19,59 +20,90 @@ function getProfiles(json) {
     const postcode = person.location.postcode;
     const birthday = person.dob.date;
     const phone = person.phone;
-    return fetch(peopleUrl)
-      .then(response => response.json())
 
-      .then(profile => {
-        return {
-          ...profile,
-          firstName,
-          lastName,
-          picture,
-          email,
-          city,
-          state,
-          postcode,
-          birthday,
-          phone, 
-          street
-        };
-      })
-      .catch(err => console.log('Error fetching random user', err));
+    profileArr.push({
+      firstName,
+      lastName,
+      picture,
+      email,
+      city,
+      state,
+      street,
+      postcode,
+      birthday,
+      phone
+    });
   });
-
-  return Promise.all(profiles);
+  return profileArr;
 }
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
-function generateHTML(data) {
-  data.map(person => {
+function generateHTML(arr) {
+  
+    arr.forEach(profile => {
     const card = document.createElement('div');
     card.className += 'card';
     gallery.appendChild(card);
     card.innerHTML = `
-            <div class="card-img-container">
-            <img class="card-img" src=${person.picture} alt="profile picture">
-            </div>
-            <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${person.firstName} ${
-      person.lastName
+          <div class="card-img-container">
+          <img class="card-img" src=${profile.picture} alt="profile picture">
+          </div>
+          <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${profile.firstName} ${
+      profile.lastName
     } </h3>
-            <p class="card-text">${person.email}</p>
-            <p class="card-text cap">${person.city}, ${person.state}</p>
-            </div>
-            </div>
-            `;
-  });
+          <p class="card-text">${profile.email}</p>
+          <p class="card-text cap">${profile.city}, ${profile.state}</p>
+          </div>
+          </div>
+          `;
 
-  const card = document.querySelector('.card');
-  card.addEventListener('click', () => {
-    generateModalWindow(data);
-  });
+    const modalContainer = document.createElement('div');
+    modalContainer.className += 'modal-container';
+    
+    modalContainer.innerHTML = `
+      <div class="modal">
+                      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                      <div class="modal-info-container">
+                          <img class="modal-img" src=${
+                            profile.picture
+                          } alt="profile picture">
+                          <h3 id="name" class="modal-name cap">${
+                            profile.firstName
+                          } ${profile.lastName}</h3>
+                          <p class="modal-text">${profile.email}</p>
+                    <p class="modal-text cap">${profile.city}</p>
+                    <hr>
+                    <p class="modal-text">${formatPhoneNumber(profile.phone)}</p>
+                    <p class="modal-text">${profile.street}, ${profile.state} ${
+      profile.postcode
+    }</p>
+                     <p class="modal-text">Birthday: ${formatBirthday(
+                       profile.birthday
+                     )}</p>
+                  </div>
+                  </div>
+      `;
+      $('#gallery').after(modalContainer)
+      $('.modal-container').css('visibility', 'hidden');
+        });
+
+        $('.card').on('click', (e) => {
+          console.log(e.target )
+        })
+        
+
 }
+/**
+ * I used this link to generate the birthday date correctly:
+ * https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
+ * I used this function to generate the phone with Regex:
+ * https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
+ */
+
 function formatPhoneNumber(phoneNumberString) {
   var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
   var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
@@ -82,50 +114,13 @@ function formatPhoneNumber(phoneNumberString) {
 }
 
 function formatBirthday(date) {
-  const newDate = new Date(date)
-  newDate.toLocaleDateString("en-US")
- return newDate
+  const newDate = new Date(date);
+  const newEvent = newDate.toLocaleDateString('en-US');
+  return newEvent;
 }
 
-/**
- * I used this link to generate the birthday date correctly:
- * https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
- * I used this function to generate the phone with Regex:
- * https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
- */
-function generateModalWindow(data) {
-  data.map(person => {
-    const modalContainer = document.createElement('div');
-    modalContainer.className += 'modal-container';
-    body.appendChild(modalContainer);
-    modalContainer.innerHTML = `
-      <div class="modal">
-                      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                      <div class="modal-info-container">
-                          <img class="modal-img" src=${
-                            person.picture
-                          } alt="profile picture">
-                          <h3 id="name" class="modal-name cap">${
-                            person.firstName
-                          } ${person.lastName}</h3>
-                          <p class="modal-text">${person.email}</p>
-                    <p class="modal-text cap">${person.city}</p>
-                    <hr>
-                    <p class="modal-text">${formatPhoneNumber(person.phone)}</p>
-                    <p class="modal-text">${person.street}, ${
-      person.state
-    } ${person.postcode}</p>
-                     <p class="modal-text">Birthday: ${person.birthday}</p>
-                  </div>
-                  </div>
-      `;
-  })
-  
 
-  $('.modal-close-btn').on('click', () => {
-    $('.modal-container').css('visibility', 'hidden');
-  });
-}
+
 
 window.addEventListener('load', () => {
   fetch(peopleUrl)
@@ -138,52 +133,3 @@ window.addEventListener('load', () => {
     });
 });
 
-// $(".open").on("click", function(){
-//   $(".popup, .popup-content").addClass("active");
-//   });
-// $(function(){
-//   $('.card').lightBox({
-//     containerResizeSpeed: 250,
-//     fixedNavigation: true
-//   });
-
-// });
-
-/* <p class="modal-text">${person.email}</p>
-                        <p class="modal-text cap">${person.city}</p>
-                        <hr>
-                        <p class="modal-text">${formatPhoneNumber(
-                          person.phone
-                        )}</p>
-                        <p class="modal-text">${person.street}, ${person.city}, ${person.state} ${person.postcode}</p>
-                         <p class="modal-text">Birthday: ${person.birthday.toLocaleDateString('en-US'
-                        )}</p>
-                    </div>
- */
-
-// data.map(person => {
-//   const modalContainer = document.createElement('div');
-// modalContainer.className += 'modal-container';
-// body.appendChild(modalContainer);
-// modalContainer.innerHTML = `
-// <div class="modal">
-//                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-//                 <div class="modal-info-container">
-//                     <img class="modal-img" src=${
-//                       person.picture
-//                     } alt="profile picture">
-//                     <h3 id="name" class="modal-name cap">${
-//                       person.firstName
-//                     } ${person.lastName}</h3>
-//                     <p class="modal-text">${person.email}</p>
-//                   <p class="modal-text cap">${person.city}</p>
-//                   <hr>
-//                   <p class="modal-text">${formatPhoneNumber(
-//                     person.phone
-//                   )}</p>
-//                   <p class="modal-text">${person.street}, ${person.city}, ${person.state} ${person.postcode}</p>
-//                    <p class="modal-text">Birthday: ${person.birthday}</p>
-//               </div>
-//             </div>
-// `;
-// });
